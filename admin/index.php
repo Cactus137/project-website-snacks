88d85d382a6b47f264aba0d0b63ddc2f9be4af69
Tnenl <anhdo13072004@gmail.com>
2023-10-30 20:25:12 +0700

    Add new project food

---
+++ admin/index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Trang quản trị</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- CSS thuần -->
+    
+    <!-- Goole font -->
+    
+    <!-- font-awesome -->
+    
+</head>
+
+<body>
+    <div class="d-flex">
+    
+        <section>
+            <header>
+            
+            </header>
+            <main>
+            
+            </main>
+        </section>
+    </div>
+    <footer>
+    
+    </footer>
+    <!-- Bootstrap JavaScript Libraries -->
+    
+    <!-- Core plugin JavaScript-->
+    
+
+    <!-- Custom scripts for all pages-->
+    
+
+    <!-- Page level plugins -->
+    
+
+    <!-- Page level custom scripts -->
+    
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Trang chủ</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.1 --> 
+    
+    <!-- CSS thuần -->
+    
+    <!-- font-awesome -->
+    
+</head>
+
+<body class="mt-5 pt-5">
+    <header>
+    
+    </header>
+    
+    <main>
+        <div class="container">
+
+            <?php
+            if () {
+                switch () {
+                    case 'home': 
+                        break;
+
+                    default: 
+                        break;
+                }
+            } else { 
+            }
+            ?>
+        </div>
+    </main>
+    
+    <footer>
+        
+    </footer>
+    <!-- Bootstrap, JavaScript Libraries -->
+    
+</body>
+
+</html>

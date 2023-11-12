f1753fa5d41f0fe64677f0c4b745ce6e47bc2653
Trungduc2807 <nguyentrungduc2807@gmail.com>
2023-11-12 22:22:59 +0700

    THử lại file products

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include './user/header.php';
-// include './user/products.php';
-include './user/pay.php';
+include './user/products.php';
+// include './user/pay.php';
 // include './user/home.php';
 include './user/footer.php';
 ?>
